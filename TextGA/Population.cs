c315719c5f2b88554a2ef3870cb98a53faea85fb
jean-tessier <jeant@clemson.edu>
2017-07-22 20:23:36 -0400

    Added IReadOnlyPopulation (adding immutability). Changed internals of Population to deal with Individual, rather than IReadOnlyIndividual (too much immutability, I think... I was starting to break it). Made Population enumerable.

--- TextGA/Population.cs
+++ TextGA/Population.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,14 +7,19 @@ using System.Threading.Tasks;
 
 namespace TextGA
 {
-    public class Population
+    public interface IReadOnlyPopulation : IEnumerable<Individual>
     {
-        private List<IReadOnlyIndividual> _pop;
+        Individual GetIndividual(int idx);
+        List<Individual> ToList();
+    }
+    public class Population : IReadOnlyPopulation
+    {
+        private List<Individual> _pop;
         private static IReadOnlyConfigClass _config = ConfigClass.getInstance();
 
         public Population(bool init = false)
         {
-            _pop = new List<IReadOnlyIndividual>();
+            _pop = new List<Individual>();
 
             if (init)
             {
@@ -29,19 +35,29 @@ namespace TextGA
             }
         }
 
-        public void AddIndividual(IReadOnlyIndividual i)
+        public void AddIndividual(Individual i)
         {
             _pop.Add(i);
         }
 
-        public IReadOnlyIndividual GetIndividual(int idx)
+        public Individual GetIndividual(int idx)
         {
             return _pop.ElementAt(idx);
         }
 
-        public List<IReadOnlyIndividual> ToList()
+        public List<Individual> ToList()
         {
             return _pop;
+        }
+
+        public IEnumerator<Individual> GetEnumerator()
+        {
+            return (IEnumerator<Individual>)_pop;
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return (IEnumerator)_pop;
         }
     }
 }
